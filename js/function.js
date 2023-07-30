fad2d6dfa4de6c6bfb6c2c9fcf696a9b920d911a
igor_walace <131620215+Igorwalace@users.noreply.github.com>
2023-07-29 21:02:19 -0300

    new

--- js/function.js
+++ js/function.js
@@ -16,15 +16,29 @@ addEventListener("scroll",function(){
         })
     })
 
-var el = document.getElementById('projeto1')
-
-el.addEventListener('click', ()=>{
-    let interno = document.querySelectorAll('.box-cobrir')
+    // projeto
+    var el = document.getElementById('projeto1')
+    el.addEventListener('click', ()=>{
+    var interno = document.querySelectorAll('.box-cobrir')
     interno[0].style.display = 'block'
 
     let link2 = document.querySelectorAll('.two h1')[0]
     link2.addEventListener('click',()=>{
         open('https://jokenpo-igor.netlify.app/')
+    })
+
+    let link3 = document.querySelectorAll('.tree h1')[0]
+    link3.addEventListener('click', ()=>{
+        let cobriNone = document.querySelectorAll('.cobrir')[0]
+        var b1 = document.querySelectorAll('.sobre-blur')[0]
+        b1.style.display = 'flex'
+        cobriNone.style.display = 'none'
+
+        document.querySelectorAll('.sobre-blur img')[0]
+        .addEventListener('click', ()=>{
+            b1.style.display = 'none'
+            cobriNone.style.display = 'flex'
+        })
     })
     
     document.querySelectorAll('.cobrir img')[0]
